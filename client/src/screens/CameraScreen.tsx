89ad8ab82ee7a60f524900011c25d39af145cf86
James Zhong <thebitspud@gmail.com>
2026-06-11 12:29:37 -0700

    Implement camera screen

--- client/src/screens/CameraScreen.tsx
+++ client/src/screens/CameraScreen.tsx
@@ -1,9 +1,39 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import {
+  useCameraDevice,
+  useCameraPermission,
+} from 'react-native-vision-camera';
+import { SkiaCamera } from 'react-native-vision-camera-skia';
 
 export function CameraScreen() {
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  if (!hasPermission) {
+    void requestPermission();
+    return <Text>Requesting camera permission...</Text>;
+  }
+
+  if (!device) {
+    return <Text>No camera found.</Text>;
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Camera screen</Text>
+    <View style={StyleSheet.absoluteFill}>
+      <SkiaCamera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive
+        pixelFormat='yuv'
+        onFrame={(frame, render) => {
+          render(({ frameTexture, canvas }) => {
+            canvas.drawImage(frameTexture, 0, 0);
+            // Add Skia filter/drawing here.
+          });
+
+          frame.dispose();
+        }}
+      />
     </View>
   );
 }
